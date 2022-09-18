6f26b8ab18f9f3022a1a94b7c16c817fc33f9c0c
valmir <valmirchagas88@gmail.com>
2022-09-18 00:51:04 -0300

    Calculate date and Perimeter of a rectangle

---
+++ Day1/03-Arithmetic Operators.js
@@ -0,0 +1,28 @@
+/*
+*-------------------------
+*       Problem Info
+*-------------------------
+* Direct Link: https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem
+* Difficulty: Easy
+* Max Score: 10
+* Language: JavaScript (Node.js)
+* ------------------------
+*         Solution
+* ------------------------
+*/
+
+
+//  Calculate the area and Perimeter of a rectangle.
+
+function getArea(length, width) {
+    let area = length * width;
+    
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter = 2 * (length + width);
+    
+    return perimeter;
+}
+
