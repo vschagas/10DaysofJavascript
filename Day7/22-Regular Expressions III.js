4f5ef5cfdf16c01a955e7b09bf71a7027211766b
valmir <valmirchagas88@gmail.com>
2022-09-28 00:10:39 -0300

    return a RegExp object

---
+++ Day7/22-Regular Expressions III.js
@@ -0,0 +1,19 @@
+/*
+
+*-------------------------
+*       Problem Info
+*-------------------------
+
+* Direct Link: https://www.hackerrank.com/challenges/js10-objects/problem
+* Difficulty: Easy
+* Max Score: 15
+* Language: JavaScript (Node.js)
+
+* ------------------------
+*         Solution
+* ------------------------
+*/
+
+function regexVar() {
+  return new RegExp(/[0-9]+/gm);
+}
