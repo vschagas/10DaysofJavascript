af580d95ea7afeea34dcd61c0d7bc2073a895c95
valmir <valmirchagas88@gmail.com>
2022-09-28 00:04:23 -0300

    Exercise to pratice regex

---
+++ Day7/20-Regular Expressions I.js
@@ -0,0 +1,18 @@
+/*
+*-------------------------
+*       Problem Info
+*-------------------------
+
+* Direct Link: https://www.hackerrank.com/challenges/js10-objects/problem
+* Difficulty: Easy
+* Max Score: 15
+* Language: JavaScript (Node.js)
+
+* ------------------------
+*         Solution
+* ------------------------
+*/
+
+function regexVar() {
+  return new RegExp(/^([aeiou]).*\1$/);
+}
