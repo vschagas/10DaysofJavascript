4b72b185c537bd18347a5c2b77b6a9d8bc9a5376
valmir <valmirchagas88@gmail.com>
2022-09-28 00:08:18 -0300

    return a RegExp object,

---
+++ Day7/21-Regular Expressions II.js
@@ -0,0 +1,19 @@
+/*
+
+*-------------------------
+*       Problem Info
+*-------------------------
+
+* Direct Link: https://www.hackerrank.com/challenges/js10-objects/problem
+* Difficulty: Easy
+* Max Score: 15
+* Language: JavaScript (Node.js)
+
+* ------------------------
+*         Solution
+* ------------------------
+*/
+
+function regexVar() {
+  return new RegExp(/^(Mr\.|Dr\.|Er\.|Ms\.|Mrs\.)\s?[a-z|A-Z]+$/);
+}
